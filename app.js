12eaef4da2d07b863a8fa702a2b9cf4f22a033ee
davidhitman <hitimanadav47@gmail.com>
2023-04-17 17:27:57 +0100

    d

--- app.js
+++ app.js
@@ -1,70 +1,65 @@
-import AWS from './aws-sdk';
+StartButton.addEventListener("click", function(){
+    const frontPage = document.getElementById('front-page');
+});
 
-const video = document.getElementById('video');
-var canvas = document.getElementById('canvas');
-const snap = document.getElementById("snap");
-const errorMsgElement = document.querySelector('span#errorMsg');
 
-const constraints = {
-    video: {
-        width: 1280, height: 720
-    }
-};
+var context = canvas.getContext('2d');
+function cameravideo(){
+    const video = document.getElementById('video');
+    var canvas = document.getElementById('canvas');
+    const snap = document.getElementById("snap");
+    const errorMsgElement = document.querySelector('span#errorMsg');
 
-// Access webcam
-async function init() {
+    const constraints = {
+        video: {
+            width: 1280, height: 720
+        }
+    };
     try {
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        handleSuccess(stream);
+        const stream = navigator.mediaDevices.getUserMedia(constraints);
+        window.stream = stream;
+        video.srcObject = stream;
     } catch (e) {
         errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
     }
-}
+};
 
-// Success
-function handleSuccess(stream) {
-    window.stream = stream;
-    video.srcObject = stream;
-}
-
-// Load init
-init();
-
-// Draw image
-var context = canvas.getContext('2d');
+    // Draw image
 snap.addEventListener("click", function() {
     context.drawImage(video, 0, 0, 640, 480);
     saveImage();
 });
 
+
+
 function saveImage(){
     var imageDataURL = canvas.toDataURL('image/png');
     var image = imageDataURL;
-    sendDataBase(image);
+    //sendDataBase(image);
     
-}
+};
 
 function sendDataBase(image){
     
-    //const AWS = require('./aws-sdk');
-    const s3 = new AWS.S3({
-        accessKeyId: 'AKIAVW5L6NSWFIE3XIDC',
-        secretAccessKey: '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'
-    });
+    // //const AWS = require('./aws-sdk');
+    // const s3 = new AWS.S3({
+    //     accessKeyId: 'AKIAVW5L6NSWFIE3XIDC',
+    //     secretAccessKey: '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'
+    // });
 
-    const params = {
-        Bucket: 'face-recognition-website',
-        Key: 'myImage.jpg',
-        Body: image // the image data captured from the camera
-    };
-    s3.putObject(params, (err, data) => {
-        if (err) {
-            console.log('Error uploading image to S3: ', err);
-        } else {
-            console.log('Successfully uploaded image to S3');
-        }
-    });
-}
+    // const params = {
+    //     Bucket: 'face-recognition-website',
+    //     Key: 'myImage.jpg',
+    //     Body: image // the image data captured from the camera
+    // };
+    // s3.putObject(params, (err, data) => {
+    //     if (err) {
+    //         console.log('Error uploading image to S3: ', err);
+    //     } else {
+    //         console.log('Successfully uploaded image to S3');
+    //     }
+    // });
+};
 
 
 
