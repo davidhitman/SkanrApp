31fddb9abff5a17f16af76f8ee9f43298428ef7a
davidhitman <hitimanadav47@gmail.com>
2023-04-17 19:46:21 +0100

    dd

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+const video = document.getElementById('video');
+const snap = document.getElementById("snap");
+const frontPage = document.getElementById('front-page');
+const cameraDiv = document.getElementById('Camera-div');
+
 StartButton.addEventListener("click", function(){
-    const frontPage = document.getElementById('front-page');
+    frontPage.style.display = "none";
+    cameravideo();
 });
 
 
-var context = canvas.getContext('2d');
-function cameravideo(){
-    const video = document.getElementById('video');
-    var canvas = document.getElementById('canvas');
-    const snap = document.getElementById("snap");
-    const errorMsgElement = document.querySelector('span#errorMsg');
-
+async function cameravideo(){
+    cameraDiv.style.display = "initial";
+   // const errorMsgElement = document.querySelector('span#errorMsg');
     const constraints = {
         video: {
             width: 1280, height: 720
         }
     };
     try {
-        const stream = navigator.mediaDevices.getUserMedia(constraints);
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
         window.stream = stream;
         video.srcObject = stream;
     } catch (e) {
@@ -24,7 +28,6 @@ function cameravideo(){
     }
 };
 
-    // Draw image
 snap.addEventListener("click", function() {
     context.drawImage(video, 0, 0, 640, 480);
     saveImage();
