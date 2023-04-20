31660146f0c7eaec249451a913e7018b592f613a
davidhitman <107502959+davidhitman@users.noreply.github.com>
2023-04-20 18:31:38 +0100

    v

--- index.js
+++ index.js
@@ -40,11 +40,6 @@ app.get("/collections/:collectionName", function (req, res, next) {
   });
 });
 
-
-
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
     console.log("App started on port: " + port);
