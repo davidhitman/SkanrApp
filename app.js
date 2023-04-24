0f0f2a300ab2aa672d63cea8a8ca7db8acaf169f
davidhitman <107502959+davidhitman@users.noreply.github.com>
2023-04-24 20:27:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ let App = new Vue({ // The Vue instance
     el: '#app',
     data: {
          user:[],
-         login:true,
-         search:'',
-         urls: "firstApplication-env-1.eba-chfmnifa.eu-west-2.elasticbeanstalk.com",
+         login:false,
+         homePage:true,
+         searchText: "",
+         urls: "firstApplication-env-1.eba-chfmnifa.eu-west-2.elasticbeanstalk.com/collections",
     },
+
+      // fetching the details from database
 
     created: function () {
         fetch("http://firstapplication-env-1.eba-chfmnifa.eu-west-2.elasticbeanstalk.com/collections/User")
@@ -14,57 +17,86 @@ let App = new Vue({ // The Vue instance
             this.user = user;
             return;
           });
-        // this.getLessons();
         return;
     },
+    
     methods:{ // methods to be used
+        async getUsers() {
+            try {
+                const url = `${this.urls}/User/search/${this.searchText}`;
+                const response = await fetch(url);
+                this.user = await response.json();
+           } catch (error) {
+                this.error = error;
+           }
+    },
         
         
-//         async getUsers() {
-//         try {
-//           const url = `${this.urls}/collections/User/search/${this.searchText}`;
-  
-//           const response = await fetch(url);
-  
-//           this.user = await response.json();
-//         } catch (error) {
-//           this.error = error;
-//         }
-//       },
-          
-//         getUsers() {
-//           const url = `${this.urls}/collections/User`;
-//           fetch(url)
-//             .then((response) => {
-//               if (!response.ok) {
-//                 throw new Error(`HTTP error! status: ${response.status}`);
-//               }
-//               return response.json();
-//             })
-//             .then((user) => {
-//               this.user = user;
-//             })
-//             .catch((Error) => {
-//               console.log("Error", Error);
-//             });
-//         },
-        logIn(){
-          a = true;
-          let email = document.getElementById("inputEmail").value;
-          let password = document.getElementById("inputPassword").value;
-          
-          for (user in this.user){
-            let storedEmail = this.user[user].Email;
-            let storedPassword = this.user[user].Password;
-            if (email===storedEmail){
-              if(password===storedPassword ){
-                this.login=false;
-              }
+      // method to check if the user needs to signUp
+      signUp(){
+        this.login=false;
+        this.homePage=false;
+      },
+      // method for logIn 
+      logIn(){
+        let email = document.getElementById("inputEmail").value;
+        let password = document.getElementById("inputPassword").value;
+        for (user in this.user){
+          let storedEmail = this.user[user].Email;
+          let storedPassword = this.user[user].Password;
+          if (email===storedEmail){
+            if(password===storedPassword ){
+              this.login=false;
+              this.homePage=true;
             }
           }
         }
-    };
-    
+      },
+      // signUp method add new user
+      addUsers (newUser){
+        fetch("http://firstapplication-env-1.eba-chfmnifa.eu-west-2.elasticbeanstalk.com/collections/User", {
+          method: "POST", //set the HTTP method as "POST"
+          headers: {
+            "Content-Type": "application/json", //set the data type as JSON
+          },
+          body: JSON.stringify(newUser) //need to stringigy the JSON
+        }).then(
+          function(response) {
+            response.json().then(
+              function(json) {
+                console.log("Success: " + json.acknowledged);
+                
+              }
+           )}
+        );
+      },
 
+        //////// sign up collection of data User Input
+      collectSignUpData(){
+        let enteredName = document.getElementById("name").value;
+        let enteredProfession = document.getElementById("profession").value;
+        let enteredEvent = document.getElementById("event").value;
+        let enteredEmail = document.getElementById("email").value;
+        let enteredPassword = document.getElementById("password").value;
+        let image = document.getElementById("image").files[0];
+
+        this.addUsers({
+          name: enteredName,
+          Profession: enteredProfession,
+          Event: enteredEvent,
+          Email: enteredEmail,
+          Password:enteredPassword
+        });
+        alert("User Has been saved");
+      }
+    },
+    watch: {
+        searchText: {
+            handler(val) {
+                this.getUsers();
+            },
+        },
+    },
 })
+//document.querySelector('a').addEventListener('click', App.signUp);
 
