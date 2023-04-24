8815c4835607dc1b9a7d4a98366af03c446e69b1
davidhitman <107502959+davidhitman@users.noreply.github.com>
2023-04-24 11:39:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -45,32 +45,32 @@ app.param("collectionName", function (req, res, next, collectionName) {
 
 //////////////////////////////////////////////////////////////////////
 
-// app.get("/collections/:collectionName/search/:query",function (req, res, next) {
-//     //const searchText = req.query.search;
-//     let searchText = req.params.query;
+app.get("/collections/:collectionName/search/:query",function (req, res, next) {
+    //const searchText = req.query.search;
+    let searchText = req.params.query;
 
-//     let query = {};
-//     query = {
-//       $or: [
-//         { name: { $regex: searchText, $options: "i" } },
-//       ],
-//     };
-//     req.collection.find(query, {}).toArray(function (err, results) {
-//       if (err) {
-//         return next(err);
-//       }
-//       res.send(results);
-//     });
-//   }
-// );
-// app.get("/collections/:collectionName/search", function (req, res, next) {
-//   req.collection.find({}).toArray(function (err, results) {
-//     if (err) {
-//       return next(err);
-//     }
-//     res.send(results);
-//   });
-// });
+    let query = {};
+    query = {
+      $or: [
+        { name: { $regex: searchText, $options: "i" } },
+      ],
+    };
+    req.collection.find(query, {}).toArray(function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      res.send(results);
+    });
+  }
+);
+app.get("/collections/:collectionName/search", function (req, res, next) {
+  req.collection.find({}).toArray(function (err, results) {
+    if (err) {
+      return next(err);
+    }
+    res.send(results);
+  });
+});
 
 
 //////////////////////////////////////////////////////////
