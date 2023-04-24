1379926ec49fe35fb121ccf09e5d9f49a4052a74
davidhitman <107502959+davidhitman@users.noreply.github.com>
2023-04-24 03:30:08 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+let App = new Vue({ // The Vue instance
+    el: '#app',
+    data: {
+         user:[],
+         login:true,
+         search:'',
+         urls: "firstApplication-env-1.eba-chfmnifa.eu-west-2.elasticbeanstalk.com",
+    },
+
+    created: function () {
+        fetch("http://firstapplication-env-1.eba-chfmnifa.eu-west-2.elasticbeanstalk.com/collections/User")
+          .then((response) => response.json())
+          .then((user) => {
+            this.user = user;
+            return;
+          });
+        // this.getLessons();
+        return;
+    },
+    methods:{ // methods to be used
+        getUsers() {
+          const url = `${this.urls}/collections/User`;
+          fetch(url)
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+              }
+              return response.json();
+            })
+            .then((user) => {
+              this.user = user;
+            })
+            .catch((Error) => {
+              console.log("Error", Error);
+            });
+        },
+        logIn(){
+          a = true;
+          let email = document.getElementById("inputEmail").value;
+          let password = document.getElementById("inputPassword").value;
+          
+          for (user in this.user){
+            let storedEmail = this.user[user].Email;
+            let storedPassword = this.user[user].Password;
+            if (email===storedEmail){
+              if(password===storedPassword ){
+                this.login=false;
+              }
+            }
+          }
+        }
+    }
+
+})
+
